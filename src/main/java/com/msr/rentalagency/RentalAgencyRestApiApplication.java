9af873bf516b10f058ccadd724a540c4dd7b5378
sogore massire <massire.org@gmail.com>
2024-01-03 19:18:16 +0100

    initialize project

---
+++ src/main/java/com/msr/rentalagency/RentalAgencyRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.msr.rentalagency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalAgencyRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalAgencyRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msr/rentalagency/RentalAgencyRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msr.rentalagency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalAgencyRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
